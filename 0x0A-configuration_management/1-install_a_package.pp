750ef0ea158c5a3355cca060834da3575530b5bc
Simangeletutubala <simangeletutubala@gmail.com>
2024-05-09 14:05:32 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 #!/usr/bin/pup
-# Installs flask from pip3, version 2.1.0
-
-package { 'flask':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
-
