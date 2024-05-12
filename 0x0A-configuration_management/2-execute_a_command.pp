3cb592992cf638ad8f7235c21f0d1bdb5de1876b
Simangeletutubala <simangeletutubala@gmail.com>
2024-05-11 22:16:54 -0700

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,9 @@
-# create a manifest that kills a process named killmenow
+#!/usr/bin/env puppet
+# Using Puppet to create a manifest that kills a process named killmenow.
 
-exec { 'pkill':
-  command  => 'pkill -f killmenow',
-  provider => 'shell',
+exec { 'kill':
+  command   => 'pkill -f killmenow',
+  path      => ['/usr/bin', '/usr/sbin'],
+  returns   => ['0', '1'],
+  logoutput => true,
 }
